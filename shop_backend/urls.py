479cbe76b3bd15dcfb5a5a12f294d1fc22dcf8ed
Fahid Hasan Fuad <96814607+Fahid24@users.noreply.github.com>
2024-02-21 18:53:41 +0600

    added new

--- shop_backend/urls.py
+++ shop_backend/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', home, name="home"),
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
